890d07f4660fb2b5a8e7856f03c846e9e48a341c
Haparandom <myfka1989@gmail.com>
2024-11-20 11:52:24 +0300

    Тестовая отправка немного зменённого стандартного проекта

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
+            <h1 id="tableLabel">Weather forecast тестирование</h1>
             <p>This component demonstrates fetching data from the server.</p>
             <Header />
             {contents}
